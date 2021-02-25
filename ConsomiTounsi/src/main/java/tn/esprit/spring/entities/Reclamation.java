0b2703ac9eb010b583e56ae2a68bb49b007ed9dd
Mohamed Farah <hamouchka7@gmail.com>
2021-02-25 10:33:29 +0100

    PushTest

---
+++ ConsomiTounsi/src/main/java/tn/esprit/spring/entities/Reclamation.java
@@ -0,0 +1,50 @@
+package tn.esprit.spring.entities;
+import java.util.*;
+
+/**
+ * 
+ */
+public class Reclamation {
+
+    /**
+     * Default constructor
+     */
+    public Reclamation() {
+    }
+
+    /**
+     * 
+     */
+    private int Id;
+
+    /**
+     * 
+     */
+    private String TypeReclamation;
+
+    /**
+     * 
+     */
+    private String Objet;
+
+    /**
+     * 
+     */
+    private boolean State;
+
+    /**
+     * 
+     */
+   // private Decision Decision;
+
+    /**
+     * 
+     */
+    private Date DateLimit;
+
+
+
+
+
+
+}

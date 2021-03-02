1154ad0404964b1e5a039d4fcdd90280a0965710
Mohamed Farah <hamouchka7@gmail.com>
2021-03-02 10:26:09 +0100

    added heritage annotation

--- ConsomiTounsi/src/main/java/tn/esprit/spring/entities/User.java
+++ ConsomiTounsi/src/main/java/tn/esprit/spring/entities/User.java
@@ -12,6 +12,8 @@ import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 import javax.persistence.OneToMany;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -20,6 +22,7 @@ import javax.persistence.Transient;
 
 
 @Entity
+@Inheritance(strategy = InheritanceType.JOINED)
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 

05efb71657b09b6540d86bfecf0fad08ebed15fc
Mohamed Farah <hamouchka7@gmail.com>
2021-03-03 20:15:39 +0100

    OpenApi annotation

--- ConsomiTounsi/src/main/java/tn/esprit/spring/ConsomiTounsiApplication.java
+++ ConsomiTounsi/src/main/java/tn/esprit/spring/ConsomiTounsiApplication.java
@@ -3,11 +3,23 @@ package tn.esprit.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 
 
 
 
 @SpringBootApplication
+@Import(springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration.class)
+@OpenAPIDefinition(
+	    info = @Info(
+	        title = "Consomi Tounsi",
+	        version = "2.0"
+	    )
+	)
+	
 public class ConsomiTounsiApplication {
 
 	public static void main(String[] args) {

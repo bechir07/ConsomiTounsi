491f4191216ea6e2704147292608680d09b6ef99
Mohamed Farah <hamouchka7@gmail.com>
2021-03-03 16:25:35 +0100

    add Swagger

--- ConsomiTounsi/src/main/java/tn/esprit/spring/ConsomiTounsiApplication.java
+++ ConsomiTounsi/src/main/java/tn/esprit/spring/ConsomiTounsiApplication.java
@@ -1,22 +1,16 @@
 package tn.esprit.spring;
 
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 
 @SpringBootApplication
-//@EnableSwagger2
 @Import(springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration.class)
 @OpenAPIDefinition(
 	    info = @Info(
